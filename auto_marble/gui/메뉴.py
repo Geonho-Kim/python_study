d38772d93811e39e7cc5f70ce368b406ecaa88f3
Geonho-Kim <loveho0901@naver.com>
2021-09-15 20:59:10 +0900

    9/15 auto marble-gui

--- auto_marble/gui/메뉴.py
+++ auto_marble/gui/메뉴.py
@@ -11,6 +11,8 @@ def create_new_file():
 
 menu = Menu(root)
 
+
+# File 메뉴
 menu_file = Menu(menu, tearoff=0)
 menu_file.add_command(label="New File", command=create_new_file)
 menu_file.add_command(label="New Window")
@@ -21,6 +23,22 @@ menu_file.add_command(label="Save All", state="disable")    # 비활성화
 menu_file.add_separator()
 menu_file.add_command(label="Exit", command=root.quit)
 menu.add_cascade(label="File", menu=menu_file)
+
+# Edit 메뉴(빈 값)
+menu.add_cascade(label="Edit")
+
+# Language 메뉴 추가 (radio 버튼을 통해서 택1)
+menu_lang = Menu(menu, tearoff=0)
+menu_lang.add_radiobutton(label="Python")
+menu_lang.add_radiobutton(label="Java")
+menu_lang.add_radiobutton(label="c++")
+menu.add_cascade(label="Language", menu=menu_lang)
+
+# View 메뉴
+manu_view = Menu(menu, tearoff=0)
+manu_view.add_checkbutton(label="Show Minimap")
+menu.add_cascade(label="View", menu=manu_view)
+
 
 root.config(menu=menu)
 root.mainloop()
--- auto_marble/gui/프레임.py
+++ auto_marble/gui/프레임.py
@@ -5,6 +5,26 @@ root.title("GUI")
 root.geometry("640x480+100+100")    # 가로 x 세로 + 초기화면위치 x좌표 + 초기회면위치 y좌표
 root.resizable(False, False)    # 창 크기 변경 불가
 
+Label(root, text="메뉴를 선택해 주세요").pack(side="top")
+
+Button(root, text="주문하기").pack(side="bottom")
+
+# 메뉴 프레임
+frame_buger = Frame(root, relief="solid", bd=1)
+frame_buger.pack(side="left", fill="both", expand=True)
+
+Button(frame_buger, text="햄버거").pack()
+Button(frame_buger, text="치즈버거").pack()
+Button(frame_buger, text="치킨버거").pack()
+
+# 음료 프레임
+frame_drink = LabelFrame(root, text="음료")
+frame_drink.pack(side="right", fill="both", expand=True)
+
+Button(frame_drink, text="콜라").pack()
+Button(frame_drink, text="사이다").pack()
+
+
 root.mainloop()
 
 
