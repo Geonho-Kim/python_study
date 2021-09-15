09b4f5fed227d9a1ebab09d704ae07de95231d62
Geonho-Kim <loveho0901@naver.com>
2021-09-15 23:29:23 +0900

    9/15 auto marble-gui

---
+++ auto_marble/gui/스크롤 바.py
@@ -0,0 +1,27 @@
+from tkinter import *
+
+root = Tk()
+root.title("Scroll Bar")
+root.geometry("640x480+100+100")    # 가로 x 세로 + 초기화면위치 x좌표 + 초기회면위치 y좌표
+
+frame = Frame(root)
+frame.pack()
+
+scrollbar = Scrollbar(frame)
+scrollbar.pack(side="right", fill="y")
+
+# set이 없으면 스크롤을 내려도 다시 올라옴
+listbox = Listbox(frame, selectmode="extended", height=10, yscrollcommand=scrollbar.set)
+
+for i in range(1, 32):  # 1~31
+    listbox.insert(END, str(i)+"일") # 1일, 2일, ...
+listbox.pack(side="left")
+
+scrollbar.config(command=listbox.yview)
+
+root.mainloop()
+
+
+
+
+
